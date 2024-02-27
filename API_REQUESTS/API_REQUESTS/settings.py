56f190fdf68b280396cf60b63a4ad25a3116cb6e
Wade Sharma <125868503+VikingsWade@users.noreply.github.com>
2024-02-27 19:25:26 +0530

    Update settings.py

--- API_REQUESTS/API_REQUESTS/settings.py
+++ API_REQUESTS/API_REQUESTS/settings.py
@@ -168,5 +168,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'schandresh24@gmail.com'
-EMAIL_HOST_PASSWORD = 'ccha nkrs wmmj qtnc'
+EMAIL_HOST_PASSWORD = ''
 
